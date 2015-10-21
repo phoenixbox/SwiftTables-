f0b2becd4522cc7db83d55e5c2c570307cde2254
Shane Rogers <shane@repllabs.com>
2015-10-20 17:13:44 -0700

    Swift tables setup

---
+++ SwiftTables/TVExampleCell.swift
@@ -0,0 +1,24 @@
+//
+//  TVExampleCell.swift
+//  SwiftTables
+//
+//  Created by Shane Rogers on 10/20/15.
+//  Copyright © 2015 Shane Rogers. All rights reserved.
+//
+
+import UIKit
+
+class TVExampleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SwiftTables/ViewController.swift
+++ SwiftTables/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
   override func viewDidLoad() {
+    var tableView: UITableView?
+    
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    tableView = UITableView(frame: view.bounds, style: .Plain)
+    if let theTableView = tableView {
+      theTableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier:"UITableViewCell")
+      
+      theTableView.dataSource = self
+//      theTableView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+      
+      view.addSubview(theTableView)
+    }
+  }
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 3
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    switch section {
+    case 0:
+      return 3
+    case 1:
+      return 5
+    case 2:
+      return 8
+    default:
+      return 0
+    }
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as UITableViewCell
+    cell.textLabel!.text = "Section \(indexPath.section), Cell \(indexPath.row)"
+    return cell
+  }
+  
+  override func prefersStatusBarHidden() -> Bool {
+    return true
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
-
